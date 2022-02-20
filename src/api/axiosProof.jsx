18cc58d16bfe260df72d4ac6bfbbfb2d85bc5f8d
Sebastian Londero <xebadevs@gmail.com>
2022-02-20 20:17:40 -0300

    Tags

--- src/api/axiosProof.jsx
+++ src/api/axiosProof.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios";
 
+let tag1 = 'pvp'
+let tag2 = 'fantasy'
+let tag3 = '3d'
+
+let tags = tag1 + '.' + tag2 + '.' + tag3
+
 let options = {
 method: 'GET',
 url: 'https://free-to-play-games-database.p.rapidapi.com/api/filter',
-params: {tag: '3d.fantasy.pvp', platform: 'pc'},
+params: {tag: tags, platform: 'pc'},
 headers: {
     'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com',
     'x-rapidapi-key': 'a747c78b0emshbb6611ca068d29cp19a88ajsnd06816a76109'
@@ -14,6 +20,8 @@ headers: {
 export default function AxiosProof(){
 
     const [posts, setPosts] = useState([])
+    let condition = true
+    let filter_search = 'a'
 
     useEffect(() => {
         axios
@@ -26,17 +34,16 @@ export default function AxiosProof(){
             })
         }, [])
 
-    let filter_search = 'z'
         
     return (
         <div>
             <h1>Games Search Filter with '{filter_search}':</h1>
             <ul>                    
-                {posts.filter(post => post.title.includes(filter_search)).map(posts => (
+                {condition && posts.filter(post => post.title.includes(filter_search)).map(posts => (
                     <li key={posts.id}>
                         {posts.title}
                     </li>
-        ))}
+                ))}
             </ul>
         </div>
     )

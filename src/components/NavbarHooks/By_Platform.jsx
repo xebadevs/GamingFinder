6c53c1e29dd2f40e50ac9225a5201d99fd1a2b16
Sebastian Londero <xebadevs@gmail.com>
2022-02-27 20:29:35 -0300

    Loading

--- src/components/NavbarHooks/By_Platform.jsx
+++ src/components/NavbarHooks/By_Platform.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios";
+import Posts from '../Posts';
+import Pagination from '../Pagination';
 
 export default function By_Release({endpoint, sort, tag, platform, title}) {
   
@@ -15,9 +17,11 @@ export default function By_Release({endpoint, sort, tag, platform, title}) {
 
   const [posts, setPosts] = useState([])
   const [loading, setLoading] = useState(true)
+  const [currentPage, setCurrentPage] = useState(1)
+  const [postsPerPage] = useState(50)
+
 
   useEffect(() => {
-
     axios
         .get(options.url, options)
         .then(res => {
@@ -30,19 +34,27 @@ export default function By_Release({endpoint, sort, tag, platform, title}) {
     }, [])
 
 
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage
+  const indexOfFirstPost = indexOfLastPost - postsPerPage
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost)
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber)
+
+
   return (    
     <div>
       <h1>{title} GAMES (PC Meanwhile)</h1>
       {loading && <h2>Loading...</h2>}
       <ul>                    
-        {posts.map(posts => (
+        {currentPosts.map(posts => (
           <li key={posts.id}>
             {posts.title}
           </li>
         ))}
       </ul>
-
-
+      <Pagination postsPerPage={postsPerPage} totalPosts={posts.length} paginate={paginate} />
     </div>
   )
 }

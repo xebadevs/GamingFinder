a861faff491be8f3be6dc4ff77da7fc6f5dd211e
Sebastian Londero <xebadevs@gmail.com>
2022-03-05 23:32:24 -0300

    Pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -8,7 +8,7 @@ const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
   }
 
   return (
-    <nav>
+    <nav className='pagination-cont'>
       <ul className='pagination'>
         {pageNumbers.map(number => (
           <li key={number} className='page-item'>
